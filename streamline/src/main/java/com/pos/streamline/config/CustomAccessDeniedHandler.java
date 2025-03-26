ff8ca1a00e71a28bff3b6eeeb5ea48630ff7f286
Jerry Endaya <vagabondendaya@gmail.com>
2025-03-26 16:24:33 +0800

    added a cors config

---
+++ streamline/src/main/java/com/pos/streamline/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.pos.streamline.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+
+    }
+}
